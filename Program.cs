e571dd66ac7c03236577f7155d90ad07dafe0c2d
JuliyaElatova <elatova_jul@mail.ru>
2022-11-30 18:47:35 +0500

    Добавили описание решения!

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string[] Array = new string[8] {"abc", "13579", "bye", "second", "May", "day", "first", "123"};
+string[] ArrayEnd = new string[8];
+void ResArrayEnd(string[] Array, string[] ArrayEnd)
+{
+    int s = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+    if(Array[i].Length <= 3)
+        {
+        ArrayEnd[s] = Array[i];
+        s++;
+        }
+    }
+}
+void PrintArray(string[] ArrayEnd)
+{Console.Write($"[ ");
+    for (int i = 0; i < ArrayEnd.Length; i++)
+    { 
+        if (ArrayEnd[i] == null) 
+        {break;} 
+        else { if (ArrayEnd[i+1] == null)
+            {Console.Write($"{ArrayEnd[i]} ");}
+            else {Console.Write($"{ArrayEnd[i]}, ");}
+            }
+}   
+Console.Write($"]");
+}
+ ResArrayEnd(Array, ArrayEnd);
+ PrintArray(ArrayEnd);
